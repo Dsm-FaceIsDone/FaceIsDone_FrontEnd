5899880ec20de8c0830e13e34e86ee5c69788665
은지 <yeochinlebelomgeole@gmail.com>
2023-11-29 22:50:37 +0900

    feat :: 회원가입 페이지 퍼블리싱

---
+++ src/assets/index.js
@@ -0,0 +1,12 @@
+export { default as Main } from "./img/Main.png";
+export { default as Face } from "./img/Face.png";
+export { default as Logo } from "./img/Logo.png";
+export { default as Home } from "./img/Home.png";
+export { default as Plus } from "./img/Plus.png";
+export { default as Notice } from "./img/Notice.png";
+export { default as MyPost } from "./img/MyPost.png";
+export { default as MyEdit } from "./img/MyEdit.png";
+export { default as Heart } from "./img/Heart.png";
+export { default as NoPost } from "./img/NoPost.png";
+export { default as Profile } from "./img/Profile.png";
+export { default as NotFound } from "./img/NotFound.png";
