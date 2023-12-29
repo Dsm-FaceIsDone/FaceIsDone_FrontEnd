6ffd31a63da87ad23178e5f32a70e7f817f90f6b
은지 <yeochinlebelomgeole@gmail.com>
2023-12-29 17:26:33 +0900

    feat :: 사이드 바 코드 수정 및 로그아웃 추가

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,6 +3,7 @@ export { default as Face } from "./img/Face.png";
 export { default as Logo } from "./img/Logo.png";
 export { default as Home } from "./img/Home.png";
 export { default as Plus } from "./img/Plus.png";
+export { default as Logout } from "./img/Logout.png";
 export { default as Notice } from "./img/Notice.png";
 export { default as MyPost } from "./img/MyPost.png";
 export { default as MyEdit } from "./img/MyEdit.png";
